9e4cd43501eb201a11505e9e581009e788f823a5
Richard <ric17101raguine@gmail.com>
2023-01-23 21:03:21 +0800

    add ReverString unit test

---
+++ src/test/kotlin/ReverseStringTest.kt
@@ -0,0 +1,24 @@
+import medium.FindAllDuplicates
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class ReverseStringTest {
+    private lateinit var test: ReverseString
+
+    @BeforeEach
+    fun configureFindAllDuplicates() {
+        test = ReverseString()
+    }
+
+    @Test
+    @DisplayName("Should reverse string input")
+    fun shouldReverseString() {
+        test.reverseString("Hello ".toCharArray())
+    }
+
+    @Test
+    fun shouldReverseString2() {
+        test.reverseString("Test1 ".toCharArray())
+    }
+}

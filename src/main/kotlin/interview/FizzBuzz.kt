fc2582b4b7f434c2909bf0c290c3917493a6081b
Richard <ric17101raguine@gmail.com>
2023-02-03 08:25:27 +0800

    add FizzBuzz

--- src/main/kotlin/interview/FizzBuzz.kt
+++ src/main/kotlin/interview/FizzBuzz.kt
@@ -1,4 +1,25 @@
 package interview
 
+fun main() {
+
+//    print(FizzBuzz().print((1..100)))
+    print((1..100).map { index ->
+        mapOf(
+            0 to index,
+            index % 3 to "Fizz",
+            index % 5 to "Buzz",
+            index % 15 to "Fizz Buzz"
+        )[0]
+    })
+}
+
 class FizzBuzz {
+//    fun print(intRange: IntRange): List<Map<Int, Any>> {
+//        return intRange.map { index -> mapOf(0 to index,
+//            index % 3 to "Fizz",
+//            index % 5 to "Buzz",
+//            index % 15 to "Fizz Buzz"
+//        )[0]
+//        }
+//    }
 }

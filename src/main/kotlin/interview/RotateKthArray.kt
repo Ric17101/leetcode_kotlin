e8dbf55c8ec3d400bbd1989a8e858ea0f9d013c6
Richard <ric17101raguine@gmail.com>
2023-02-03 08:40:52 +0800

    add TODO with Rotate kth array

--- src/main/kotlin/interview/RotateKthArray.kt
+++ src/main/kotlin/interview/RotateKthArray.kt
@@ -2,11 +2,18 @@ package interview
 
 fun main() {
 
-    val string = "Reverse word in a string"
+    val list = listOf(1,2,3,4,5,6,7)
+//    list.fo
+//
+//    val reversed = string.split(" ")
+//        .reversed()
+//        .joinToString(" ")
+//
+//    print(reversed)
+}
 
-    val reversed = string.split(" ")
-        .reversed()
-        .joinToString(" ")
-
-    print(reversed)
+// TODO:
+fun IntArray.shiftLeft(k: Int) {
+    val shiftedArray = this.copyOf()
+//    var
 }
